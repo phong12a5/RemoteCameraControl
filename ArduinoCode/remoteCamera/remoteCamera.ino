ceee5fbf8ff6eb5227ecec8da6ace6ca3e9279b2
PhongDB <phong12a5@gmail.com>
2018-06-10 00:33:09 +0700

    [REMOTE_CAMERA] Programing for Arduino

--- ArduinoCode/remoteCamera/remoteCamera.ino
+++ ArduinoCode/remoteCamera/remoteCamera.ino
@@ -1,207 +1,341 @@
-/* Remote camera control 
- * Author: Dang Ba Phong
- * Created: 09/06/2018
- * email: phong12a5@gmail.com
- */
-
- /*
-          Arduino UNO with W5100 Ethernetshield or  W5100 Ethernet module, used as MQTT client
-          It will connect over Wifi to the MQTT broker and controls a digital output (LED, relay)
-          and gives the Temperature and Humidity, as well as the state of some switches
-          The topics have the format "home/br/sb" for southbound messages and  "home/nb" for northbound messages
-          Southbound are messages going to the client, northbound are messages coming from the client
-          As the available memory of a UNO  with Ethernetcard is limited, I have kept the topics short
-          Also, the payloads  are kept short
-          The Northbound topics are
-          home/br/nb/temp  for temperature
-          home/br/nb/humid  for humidity
-          home/br/nb/deur  for a door switch
-          home/br/nb/l for  the lightintensity
-          home/br/nb/pr  for the status of a PIR sensor
-          home/br/nb/ip showing the IP number of the client
-          home/br/nb/relay showing the relaystate
-
-          There is only one southbound topic:
-          home/br/sb
-          The payload here determines the action:
-          0 -Switch the relay off
-          1-Switch the  relay on
-          2-Publish the IP number of the client
-          3 Ask for the relaystate
-
-          On Startup, the Client publishes the IP number
-
+/* Remote camera control
+   Author: Dang Ba Phong
+   Created: 09/06/2018
+   email: phong12a5@gmail.com
 */
+
 #include <Ethernet.h>
 #include "PubSubClient.h"
-#include "DHT.h"
-//kennelijk  geeft update van DHT sensor library boven 1.2.1 een fout
-#define CLIENT_ID       "Hal"
-//#define TOPIC           "temp"
-#define PUBLISH_DELAY   3000
-#define PUB_TOPIC "my_username/f/temperature" //Adafruit dashboard
-#define PUB_TOPIC_h "my_username/f/humidity" //Adafruit dashboard
-#define DHTPIN          3
-#define DHTTYPE         DHT11
-#define ledPin 13
-#define relayPin 8
-String ip = "";
-bool statusKD = HIGH;
-bool statusBD = HIGH;
-bool statusGD = HIGH;
-bool relaystate = LOW;
-bool pir = LOW;
-bool startsend = HIGH;
-int lichtstatus;
-uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x06};
 
+#define CLIENT_ID                       "100"
+#define MQTT_HOST                       "m12.cloudmqtt.com"
+#define MQTT_USER                       "fgnqzkic"
+#define MQTT_PASSWD                     "zDISWheFKSN5"
+#define MQTT_PORT                       19044
+
+#define PUB_TOPIC_DISCONECTED           "DISCONECTED"
+#define PUB_TOPIC_CONNECTED             "CONNECTED"
+#define PUB_TOPIC_RESP_DATA             "CAMERA_00_RESP"
+
+#define SUB_TOPIC_INIT_DATA             "REQUESTDATA"
+#define SUB_TOPIC_REQ_CHANGE_POS        "CAMERA_00_REQ"
+
+#define DATA_POS_0                      "0"
+#define DATA_POS_1                      "1"
+#define DATA_POS_2                      "2"
+#define DATA_POS_3                      "3"
+#define DATA_POS_4                      "4"
+
+#define PIN_POS_0                       5
+#define PIN_POS_1                       6
+#define PIN_POS_2                       7
+#define PIN_POS_3                       8
+#define PIN_POS_4                       9
+
+#define PIN_MOVING_DOWN                 2
+#define PIN_MOVING_UP                   3
+
+#define READPIN_DELAY_TIMEOUT           30
+#define LOG(a)                          Serial.print(a)
+#define LOG_LN(a)                       Serial.println(a)
+
+String ip = "";
+uint8_t mac[6] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+long previousMillis = 0;
+unsigned int requestedPosition = -1;
+bool isMoving = false;
 EthernetClient ethClient;
 PubSubClient mqttClient;
-DHT dht(DHTPIN, DHTTYPE);
 
-long previousMillis;
+void initProgram();
+int getActivedPin();
+void movingToPosition(int pos);
+void responseActivedPosToDevice(int pos);
+void sendDataToBroker(const char* topic,const char* data, bool retained = false);
+void connectToBroker();
+void callback(char* topic, byte* payload, unsigned int length);
+void movingUp();
+void movingDown();
+void stopMoving();
 
 void setup() {
-  pinMode(4, INPUT_PULLUP);
-  pinMode(5, INPUT_PULLUP);
-  pinMode(6, INPUT_PULLUP);
-  pinMode(7, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(relayPin, OUTPUT);
+    LOG_LN(">>> Welcome to MQTT Arduino Project <<<");
+    // setup serial communication
+    Serial.begin(9600);
+    while (!Serial) {};
+    LOG_LN(">>> Setup PIN <<<");
+    pinMode(PIN_POS_0,INPUT_PULLUP);
+    pinMode(PIN_POS_1,INPUT_PULLUP);
+    pinMode(PIN_POS_2,INPUT_PULLUP);
+    pinMode(PIN_POS_3,INPUT_PULLUP);
+    pinMode(PIN_POS_4,INPUT_PULLUP);
+    pinMode(PIN_MOVING_DOWN,OUTPUT);
+    pinMode(PIN_MOVING_DOWN,OUTPUT);
 
-  // setup serial communication
+    // setup ethernet communication using DHCP
+    if (Ethernet.begin(mac) == 0) {
+        LOG_LN(">>> Unable to configure Ethernet using DHCP <<< ");
+        for (;;);
+    }
 
-  Serial.begin(9600);
-  while (!Serial) {};
-  Serial.println(F("MQTT Arduino Demo"));
-  Serial.println();
+    LOG(">>> Ethernet configured via DHCP <<<");
+    LOG_LN(">>> IP address: ");
+    LOG(Ethernet.localIP());
+    LOG_LN(" <<<");
 
-  // setup ethernet communication using DHCP
-  if (Ethernet.begin(mac) == 0) {
-    //Serial.println(F("Unable to configure Ethernet using DHCP"));
-    for (;;);
-  }
+    // setup mqtt client
+    mqttClient.setClient(ethClient);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    mqttClient.setCallback(callback);
+    connectToBroker();
+    LOG_LN(">>> Connected <<<");
 
-  Serial.println(F("Ethernet configured via DHCP"));
-  Serial.print("IP address: ");
-  Serial.println(Ethernet.localIP());
-  Serial.println();
-  /*
-    Serial.println(Ethernet.localIP()[0]);
-    Serial.println(Ethernet.localIP()[1]);
-    Serial.println(Ethernet.localIP()[2]);
-    Serial.println(Ethernet.localIP()[3]);
-  */
-  ip = String (Ethernet.localIP()[0]);
-  ip = ip + ".";
-  ip = ip + String (Ethernet.localIP()[1]);
-  ip = ip + ".";
-  ip = ip + String (Ethernet.localIP()[2]);
-  ip = ip + ".";
-  ip = ip + String (Ethernet.localIP()[3]);
-  //Serial.println(ip);
-
-  // setup mqtt client
-  mqttClient.setClient(ethClient);
-  //mqttClient.setServer(mqttServer, 1883);
-  // mqttClient.setServer("test.mosquitto.org", 1883);
-  //  mqttClient.setServer( "raspberrypi.local",1883);
-  //mqttClient.setServer("io.adafruit.com",1883);
-  mqttClient.setServer( "192.168.1.102", 1883);
-  //Serial.println(F("MQTT client configured"));
-  mqttClient.setCallback(callback);
-  // setup DHT sensor
-  dht.begin();
-  Serial.println(F("DHT sensor initialized"));
-
-  Serial.println();
-  Serial.println(F("Ready to send data"));
-  previousMillis = millis();
-  mqttClient.publish("home/br/nb/ip", ip.c_str());
+    // Init Application
+    initProgram();
 }
 
 void loop() {
+    //  if (millis() - previousMillis > PUBLISH_DELAY) {
+    //    previousMillis = millis();
+    //
+    //  }
+    int activedPos = getActivedPin();
+    if(isMoving)
+    {
+        if(requestedPosition == activedPos)
+        {
+            stopMoving();
+        }
+    }else{
+        if(requestedPosition >= 0 && requestedPosition != activedPos)
+        {
+            movingToPosition(requestedPosition);
+        }
+    }
 
-  statusBD = digitalRead(4);// FrontdoorSwitch
-  statusGD = digitalRead(5);// Garagedoor Switch
-  statusKD = (digitalRead(6));//LivingRoom Switch
-
-  lichtstatus = analogRead(A0);//Reads an LDR
-  pir = digitalRead(7);//Reads a PIR sensor
-  relaystate = digitalRead(relayPin);// Reads the state of a relay
-
-  // it's time to send new data?
-  if (millis() - previousMillis > PUBLISH_DELAY) {
-    sendData();
-    previousMillis = millis();
-
-  }
-
-  mqttClient.loop();
+    if(!mqttClient.connected())
+    {
+        connectToBroker();
+    }
+    mqttClient.loop();
 }
 
-void sendData() {
-
-  char msgBuffer[20];
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println("oC");
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.println("%");
-  Serial.print("Relay is: ");
-  Serial.println((relaystate == LOW) ? "OPEN" : "CLOSED");
-  if (mqttClient.connect(CLIENT_ID)) {
-    mqttClient.publish("home/br/nb/temp", dtostrf(t, 6, 2, msgBuffer));
-    mqttClient.publish("home/br/nb/humid", dtostrf(h, 6, 2, msgBuffer));
-    mqttClient.publish("home/br/nb/deur", (statusBD == HIGH) ? "OPEN" : "CLOSED");
-    mqttClient.publish("home/br/nb/garage", (statusGD == HIGH) ? "OPEN" : "DICHT");
-    mqttClient.publish("home/br/nb/bel", (statusKD == HIGH) ? "OPEN" : "CLOSED");
-    mqttClient.publish("home/br/nb/l", dtostrf(lichtstatus, 4, 0, msgBuffer));
-    mqttClient.publish("home/br/nb/p", (pir == HIGH) ? "OPEN" : "CLOSED");
-    mqttClient.publish("home/br/nb/relay", (relaystate == LOW) ? "OPEN" : "CLOSED");
-    mqttClient.subscribe("home/br/sb");
-    if (startsend) {
-     // mqttClient.publish("home/br/nb/relay", (relaystate == LOW) ? "OPEN" : "CLOSED");
-      mqttClient.publish("home/br/nb/ip", ip.c_str());
-      startsend = LOW;
+void initProgram(){
+    LOG_LN(">>> Init Application <<<");
+    int actviedPos = getActivedPin();
+    sendDataToBroker(static_cast<const char *>(PUB_TOPIC_CONNECTED),static_cast<const char *>(CLIENT_ID),false);
+    if(actviedPos >= PIN_POS_0 && actviedPos <= PIN_POS_4)
+    {
+        responseActivedPosToDevice(actviedPos);
     }
-  }
+}
+
+int getActivedPin()
+{
+    int count = 0;
+    int activedPos = -1;
+    if(!digitalRead(PIN_POS_0))
+    {
+        count ++;
+        activedPos = PIN_POS_0;
+    }
+    if(!digitalRead(PIN_POS_1))
+    {
+        count ++;
+        activedPos = PIN_POS_1;
+    }
+    if(!digitalRead(PIN_POS_2))
+    {
+        count ++;
+        activedPos = PIN_POS_2;
+    }
+    if(!digitalRead(PIN_POS_3))
+    {
+        count ++;
+        activedPos = PIN_POS_3;
+    }
+    if(!digitalRead(PIN_POS_4))
+    {
+        count ++;
+        activedPos = PIN_POS_4;
+    }
+
+    if(count > 1)
+    {
+        LOG_LN(">>> Have more 1 Actived pos! <<<");
+        activedPos = -1;
+    }else if(count <= 0)
+    {
+        LOG_LN(">>> There isn't any actived position! <<<");
+        activedPos = -1;
+    }
+    return activedPos;
+}
+
+void responseActivedPosToDevice(int pos)
+{
+    LOG_LN(">>> responseActivedPosToDevice <<<");
+    int actviedPos = getActivedPin();
+    if(actviedPos >= PIN_POS_0 && actviedPos <= PIN_POS_4)
+    {
+        switch (actviedPos) {
+        case PIN_POS_0:
+            sendDataToBroker(static_cast<const char *>(PUB_TOPIC_RESP_DATA),static_cast<const char *>(DATA_POS_0), true);
+            break;
+        case PIN_POS_1:
+            sendDataToBroker(static_cast<const char *>(PUB_TOPIC_RESP_DATA),static_cast<const char *>(DATA_POS_1), true);
+            break;
+        case PIN_POS_2:
+            sendDataToBroker(static_cast<const char *>(PUB_TOPIC_RESP_DATA),static_cast<const char *>(DATA_POS_2), true);
+            break;
+        case PIN_POS_3:
+            sendDataToBroker(static_cast<const char *>(PUB_TOPIC_RESP_DATA),static_cast<const char *>(DATA_POS_3), true);
+            break;
+        case PIN_POS_4:
+            sendDataToBroker(static_cast<const char *>(PUB_TOPIC_RESP_DATA),static_cast<const char *>(DATA_POS_4), true);
+            break;
+        default:
+            break;
+        }
+    }
+}
+
+void sendDataToBroker(const char* topic,const  char* data, bool retained)
+{
+    if(topic != nullptr && data != nullptr)
+    {
+        LOG(">>> sendDataToBroker >>> topic: ");
+        LOG(topic);
+        LOG(" >>> data: ");
+        LOG(data);
+        LOG_LN(" <<<");
+        mqttClient.publish(topic,data,retained);
+    }
+    else
+    {
+        LOG(">>> sendDataToBroker : Data is invalid <<<");
+    }
+}
+
+void connectToBroker()
+{
+    while (!mqttClient.connected())
+    {
+        LOG_LN(">>> Waiting for connect to Broker <<<");
+        delay(500);
+        mqttClient.connect(CLIENT_ID, MQTT_USER, MQTT_PASSWD, PUB_TOPIC_DISCONECTED, MQTTQOS1, true, CLIENT_ID);
+    }
+    mqttClient.subscribe(SUB_TOPIC_INIT_DATA);
+    mqttClient.subscribe(SUB_TOPIC_REQ_CHANGE_POS);
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  char msgBuffer[20];
-  // I am only using one ascii character as command, so do not need to take an entire word as payload
-  // However, if you want to send full word commands, uncomment the next line and use for string comparison
-   //payload[length] = '\0';            // terminate string with '0'
-  //String strPayload = String((char*)payload);  // convert to string
-  // Serial.println(strPayload); //can use this if using longer southbound topics
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");//MQTT_BROKER
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-  Serial.println(payload[0]);
+    LOG(">>> Message arrived [");
+    LOG(topic);
+    LOG("] <<<");//MQTT_BROKER
+    if(length>0)
+    {
+        if(strcmp(topic,static_cast<const char*>(SUB_TOPIC_REQ_CHANGE_POS)) == 0)
+        {
+            if(payload[0] == '0')
+            {
+                LOG_LN(">>> Requested position: ");
+                LOG(DATA_POS_0);
+                LOG_LN(" <<<");
+                movingToPosition(PIN_POS_0);
+                requestedPosition = PIN_POS_0;
+            }else if(payload[0] == '1')
+            {
+                LOG_LN(">>> Requested position: ");
+                LOG(DATA_POS_1);
+                LOG_LN(" <<<");
+                movingToPosition(PIN_POS_1);
+                requestedPosition = PIN_POS_1;
+            }
+            else if(payload[0] == '2')
+            {
+                LOG_LN(">>> Requested position: ");
+                LOG(DATA_POS_2);
+                LOG_LN(" <<<");
+                movingToPosition(PIN_POS_2);
+                requestedPosition = PIN_POS_2;
+            }
+            else if(payload[0] == '3')
+            {
+                LOG_LN(">>> Requested position: ");
+                LOG(DATA_POS_3);
+                LOG_LN(" <<<");
+                movingToPosition(PIN_POS_3);
+                requestedPosition = PIN_POS_3;
+            }
+            else if(payload[0] == '4')
+            {
+                LOG_LN(">>> Requested position: ");
+                LOG(DATA_POS_4);
+                LOG_LN(" <<<");
+                movingToPosition(PIN_POS_4);
+                requestedPosition = PIN_POS_4;
+            }
+        }
+        else if(strcmp(topic,static_cast<const char*>(SUB_TOPIC_INIT_DATA)) == 0)
+        {
+            int activedPos = getActivedPin();
+            if(activedPos >= PIN_POS_0 && activedPos <= PIN_POS_4)
+            {
+                responseActivedPosToDevice(activedPos);
+            }
+        }
+    }
+}
 
-  // Examine only the first character of the message
-  if (payload[0] == 49)             // Message "1" in ASCII (turn output ON)
-  {
-    digitalWrite(LED_BUILTIN, HIGH);    //
-    digitalWrite(relayPin, HIGH);
-  } else if (payload[0] == 48)      // Message "0" in ASCII (turn output OFF)
-  {
-    digitalWrite(relayPin, LOW);     //
-    digitalWrite(LED_BUILTIN, LOW);
-  } else if (payload[0] == 50)
-  {
-    mqttClient.publish("home/br/nb/ip", ip.c_str());// publish IP nr
-  } else {
-    Serial.println("Unknown value");
-    mqttClient.publish("home/br/nb", "Syntax Error");
-  }
+void movingToPosition(int pos)
+{
+    int activedPos = getActivedPin();
+    if(activedPos >= PIN_POS_0 && activedPos <= PIN_POS_4 && pos >= PIN_POS_0 && pos <= PIN_POS_4)
+    {
+        if(activedPos == pos)
+        {
+            LOG_LN("Requested position is already actived!");
+            return;
+        }
+        switch (activedPos) {
+        case PIN_POS_0:
+            movingUp();
+            break;
+        case PIN_POS_1:
+            if(pos == PIN_POS_0) movingDown();
+            else movingUp();
+            break;
+        case PIN_POS_2:
+            if(pos < PIN_POS_2) movingDown();
+            else movingUp();
+            break;
+        case PIN_POS_3:
+            if(pos == PIN_POS_4) movingUp();
+            else movingDown();
+            break;
+        case PIN_POS_4:
+            movingDown();
+            break;
+        }
+    }
+}
 
+void movingUp(){
+    LOG_LN(">>> Moving up <<<");
+    digitalWrite(PIN_MOVING_DOWN,LOW);
+    digitalWrite(PIN_MOVING_UP,HIGH);
+    isMoving = true;
+}
+void movingDown(){
+    LOG_LN(">>> Moving down <<<");
+    digitalWrite(PIN_MOVING_UP,LOW);
+    digitalWrite(PIN_MOVING_DOWN,HIGH);
+    isMoving = true;
+}
+void stopMoving(){
+    LOG_LN(">>> Stopped moving<<<");
+    digitalWrite(PIN_MOVING_UP,LOW);
+    digitalWrite(PIN_MOVING_DOWN,LOW);
+    isMoving = false;
 }
